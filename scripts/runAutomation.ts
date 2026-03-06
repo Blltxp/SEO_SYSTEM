9e79c8273e6f03fd0c910e32571d7b1439e8ddd4
Bllwtxp <tanatatpna@gmail.com>
2026-03-06 18:27:28 +0700

    Update runAutomation : add save to db

--- scripts/runAutomation.ts
+++ scripts/runAutomation.ts
@@ -1,22 +1,42 @@
 import { fetchAllSitesPosts } from "../lib/wordpress"
+import { db } from "../lib/db"
 
 async function runAutomation() {
+
   console.log("Starting SEO scan...\n")
 
   const data = await fetchAllSitesPosts()
 
   for (const site in data) {
+
     const posts = data[site]
 
     console.log(`\nSite: ${site}`)
     console.log(`Posts found: ${posts.length}`)
 
-    posts.slice(0, 3).forEach((post) => {
-      console.log("-", post.title.rendered)
-    })
+    for (const post of posts) {
+
+      const title = post.title.rendered
+      const content = post.content.rendered
+      const url = post.link
+
+      db.prepare(`
+        INSERT OR IGNORE INTO posts
+        (title, content, url, source)
+        VALUES (?, ?, ?, ?)
+      `).run(
+        title,
+        content,
+        url,
+        site
+      )
+
+    }
+
   }
 
   console.log("\nScan complete.")
+
 }
 
 runAutomation()
